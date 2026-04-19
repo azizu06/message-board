6bcfe89d7d80dc3b979f2e404c48d730a77c87d4
azizu06 <ab725492@ucf.edu>
2026-04-19 18:52:11 -0400

    simplify everything so routes are all handled at the index route

--- routes/app.js
+++ routes/app.js
@@ -7,14 +7,10 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const msgRouter = require("./newMsg");
 const indexRouter = require("./index");
-const detailRouter = require("./msgDetail");
 const PORT = 3000;
 
 app.use("/", indexRouter);
-app.use("/new", msgRouter);
-app.use("/msgs", detailRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
