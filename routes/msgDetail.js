05196ed0e3fd5c61c2a72e4d5b5da9dc52ab214d
azizu06 <ab725492@ucf.edu>
2026-04-19 18:33:22 -0400

    init msg detail route with view

---
+++ routes/msgDetail.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const router = Router();
+const messages = require("../db/messages");
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const msg = messages[id];
+  res.render("msgCard", { msg });
+});
+
+module.exports = router;
