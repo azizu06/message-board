024a2ae958c969b34704fb5c838611860322f333
azizu06 <ab725492@ucf.edu>
2026-04-22 13:41:58 -0400

    create db init script

--- db/init.js
+++ db/init.js
@@ -1,5 +1,24 @@
 require("dotenv").config();
 const { Client } = require("pg");
-module.exports = new Client({
-  connectionString: process.env.DB_URL,
-});
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    text TEXT,
+    username VARCHAR(255),
+    added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+INSERT INTO messages (text,username)
+VALUES
+    ('Hi chud', 'Aziz'),
+    ('AI is gonna take ur job', 'Techbro');
+`;
+
+const main = async () => {
+  const client = new Client({
+    connectionString: process.env.DB_URL,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+};
+main();
