40f3c23099b928c39c3bdce9f1b9b1cc44dabeec
azizu06 <ab725492@ucf.edu>
2026-04-22 14:03:40 -0400

    split up routes into controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,26 @@
 const db = require("../db/queries");
+
+exports.getMsgs = async (req, res) => {
+  const messages = await db.getAll();
+  res.render("index", { messages });
+};
+
+exports.createMsgGet = (req, res) => {
+  res.render("form");
+};
+
+exports.createMsgPost = async (req, res) => {
+  const { author, message } = req.body;
+  await db.insertMsg(message, author);
+  res.redirect("/");
+};
+
+exports.getMsgDetail = async (req, res) => {
+  const id = Number(req.params.id);
+  const rows = db.msgCnt();
+  if (!Number.isInteger(id) || id < 0 || id >= rows) {
+    return res.status(404).send("Message not found");
+  }
+  const msg = db.findMsg(id);
+  res.render("msgDetail", { msg });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,10 @@
 const { Router } = require("express");
 const router = Router();
+const controller = require("../controllers/index.js");
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
-router.get("/", (req, res) => {
-  res.render("index", { messages });
-});
-
-router.get("/new", (req, res) => {
-  res.render("form");
-});
-
-router.post("/new", (req, res) => {
-  const { author, message } = req.body;
-  messages.push({ text: message, user: author, added: new Date() });
-  res.redirect("/");
-});
-
-router.get("/msgs/:id", (req, res) => {
-  const id = Number(req.params.id);
-  if (!Number.isInteger(id) || id < 0 || id >= messages.length)
-    return res.status(404).send("Message not found");
-  const msg = messages[id];
-  res.render("msgDetail", { msg });
-});
+router.get("/", controller.getMsgs);
+router.get("/new", controller.createMsgGet);
+router.post("/new", controller.createMsgPost);
+router.get("/msgs/:id", controller.getMsgDetail);
 
 module.exports = router;
